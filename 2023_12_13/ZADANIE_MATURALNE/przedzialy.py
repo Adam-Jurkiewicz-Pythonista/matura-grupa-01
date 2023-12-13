9ddf50be3461f0d3cd720c6e0766f5794f1a140a
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-12-13 19:58:51 +0100

    wczytanie i parsowanie danych

--- 2023_12_13/ZADANIE_MATURALNE/przedzialy.py
+++ 2023_12_13/ZADANIE_MATURALNE/przedzialy.py
@@ -7,14 +7,32 @@ def parsuj(od, do):
         c += 1
     return [ x for x in range(b,c)]
 
+def odd(number):
+    if number % 2 == 1:
+        return True
+    return False
+
 with open("przedzialy.txt", "r") as fl:
     dane_prz1 = fl.readlines()
 
-# dane wczytane z pliku
-print(dane_prz1)
+# print(dane_prz1)
 
-# generowanie list z przedziałami
+lista_przedzialow = []
 for element in dane_prz1:
     e1, e2 = element.strip().split(",")
     lista = parsuj(e1,e2)
-    print(lista)
+    lista_przedzialow.append(lista)
+
+# print(lista_przedzialow)
+
+max_odd = 0
+max_line = 0
+for idx, value in enumerate(lista_przedzialow):
+    max_odd_l = len([ x for x in value if odd(x) == 1 ])
+    print(f"{idx=} {max_odd_l=} {value=}")
+    if max_odd_l > max_odd:
+        max_odd = max_odd_l
+        max_line = idx + 1
+
+print(f"Najwięcej liczb nieparzystych {max_odd} w wierszu {max_line} = {lista_przedzialow[max_line -1]}")
+
