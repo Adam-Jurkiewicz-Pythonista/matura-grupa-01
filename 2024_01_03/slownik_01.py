f397c2a0b5f6751dd1ed9107c661efb3fbe7f698
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-01-03 18:48:06 +0100

    zadanie 1.3

--- 2024_01_03/slownik_01.py
+++ 2024_01_03/slownik_01.py
@@ -1,10 +1,18 @@
 slownik = {
     1: "Linux",
     "Adam": 222,
-
+    3: "System",
+    4: 1.05,
+    4.05: 1,
     1.04: "Linux",
     "B": "cosik",
 }
 print(slownik)
 print(slownik.keys())
 print(slownik.values())
+
+slownik[4] = 2222 # to jest aktualizacja wartości istniejącego klucza
+slownik[44] = 2222333 # to jest zupełnie nowe, więc na końcu
+
+for elem in slownik:
+    print(f"{elem=} / {slownik[elem]=}")
