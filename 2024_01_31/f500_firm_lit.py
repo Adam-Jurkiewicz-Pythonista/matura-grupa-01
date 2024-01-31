2b2b83177b408ec0ed3ae3ad62b1f79a877d2b93
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-01-31 19:49:39 +0100

    2024_01_31

---
+++ 2024_01_31/f500_firm_lit.py
@@ -0,0 +1,41 @@
+import csv  # https://docs.python.org/3/library/csv.html
+
+companies = {}
+
+with open("f500.csv", "r", encoding="UTF-8") as f500:
+    csvreader = csv.reader(f500)
+
+    print(f"{csvreader=}")
+    for i, element in enumerate(csvreader):
+        if i > 0:
+            letter = element[0][0].upper()
+            if letter not in companies:
+                companies[letter] = 1
+            else:
+                companies[letter] += 1
+        # print(f"{i=} || {element=}")
+
+    # print(f"{companies['A']=} {companies['D']=} {companies['E']=}")
+    print(f"{companies=}")
+
+    # sposób okrężny
+    max_comp = max(companies.values())
+    comps = list(companies.values())
+    max_pos = comps.index(max_comp)
+    max_lett = list(companies.keys())
+    max_letter = max_lett[max_pos]
+    print(f"{max_pos}")
+    print(f"{comps=} | {max_comp=}")
+    print(f"{max_lett=}")
+    print(f"{max_letter=}")
+
+    # sposób wprost idealny na maturę - wyszukiwanie max elem. w zbiorze
+    max_companies = 0
+    for element in companies:
+        number = companies[element]
+        if number > max_companies:
+            max_companies = number
+            letter_of_max_companies = element
+    print(f"{letter_of_max_companies=} | {max_companies=}")
+
+
