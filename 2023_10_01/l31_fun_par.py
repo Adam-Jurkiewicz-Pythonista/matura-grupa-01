72cb065c43b7340d2150817cf949412528ce6044
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-02-21 19:50:32 +0100

    2023_10_01

--- 2023_10_01/l31_fun_par.py
+++ 2023_10_01/l31_fun_par.py
@@ -1,6 +1,19 @@
 def funkcja( parametr ):
     print("Podano:", parametr, "Typ to:", type(parametr))
 
+def display(number):
+    print(number)
+
+def display2( some_text ):
+    print( some_text )
+
+def display3( nic ):
+    print( nic )
+
+display(3.14)
+display2("And now for something completely different")
+display3("And now for something completely different")
+
 funkcja("aaa")
 funkcja(12)
 funkcja(12.0)
